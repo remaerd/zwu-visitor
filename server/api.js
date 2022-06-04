a71664bb5a9c25a19ea7fac1ad210cc2d9167f82
吴嘉顺 <870435170@qq.com>
2022-06-04 21:28:42 +0800

    update

--- server/api.js
+++ server/api.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection(dbConfig)
 module.exports = {
   //department
   getDepartment: (req, res, next) => {
-    connection.query(sqlMap.getDepartment, req.params, (err, result) => {
+    connection.query(sqlMap.getDepartment, req.params.id, (err, result) => {
       if (err) {
         console.log(err);
         return;
@@ -21,44 +21,5 @@ module.exports = {
         data: result
       })
     })
-  },
-  // addDepartment: (req, res, next) => {
-  //   const body = req.body;
-  //   connection.query(sqlMap.addDepartment, [body.name], (err, result) => {
-  //     if (err) {
-  //       console.log(err);
-  //       return;
-  //     }
-  //     res.json({
-  //       code: 200,
-  //       data: result
-  //     })
-  //   })
-  // },
-  // updateDepartment: (req, res, next) => {
-  //   const body = req.body;
-  //   connection.query(sqlMap.updateDepartment, [body.name, body.id], (err, result) => {
-  //     if (err) {
-  //       console.log(err);
-  //       return;
-  //     }
-  //     res.json({
-  //       code: 200,
-  //       data: result
-  //     })
-  //   })
-  // },
-  // deleteDepartment: (req, res, next) => {
-  //   const body = req.body;
-  //   connection.query(sqlMap.deleteDepartment, [body.id], (err, result) => {
-  //     if (err) {
-  //       console.log(err);
-  //       return;
-  //     }
-  //     res.json({
-  //       code: 200,
-  //       data: result
-  //     })
-  //   })
-  // }
+  }
 };
